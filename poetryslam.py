2918ff707dbc36eaf1cb7f4f27438316f04a035d
jbales20 <69781414+jbales20@users.noreply.github.com>
2020-10-03 20:04:31 -0400

    file not found

--- poetryslam.py
+++ poetryslam.py
@@ -1,15 +1,17 @@
-
+lines_list = []
 def get_file_lines(filename):
-
+    f = open(filename, "r")
+    
 
 def lines_printed_backwards(lines_list):
-
+    print("")
 
 def lines_printed_random():
-
+    print("")
 
 def lines_printed_custom(lines_list):
+    print("")
 
 
 
-
+get_file_lines("poem.txt")
